7499b9cb7ab6e468dfc212c21379db2db4ee4336
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2022-03-15 16:12:45 -0700

    go: Use JWKS for key rotation
    Instead of trying all possible pub keys one-by-one, build a map of kid
    (fingerprint) to pub key and use the kid header in jwt to select the key
    to use. If either the kid header is absent or we don't have any key with
    matching fingerprint, fallback to the first (usually current) key from
    the versioned secret.
    
    This also allows us to switch from reddit's forked version of jwt back
    to the upstream version.

--- lib/go/edgecontext/validator.go
+++ lib/go/edgecontext/validator.go
@@ -6,16 +6,37 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/reddit/baseplate.go/log"
 	"github.com/reddit/baseplate.go/secrets"
-	jwt "github.com/reddit/jwt-go/v3"
+	"golang.org/x/crypto/ssh"
 )
 
-type keysType = []*rsa.PublicKey
+type keysType struct {
+	// map of kid -> pub key.
+	m map[string]*rsa.PublicKey
+
+	// when either kid header does not exist in the jwt token,
+	// or the kid is not present in the map,
+	// we fallback to the first (usually current) key.
+	first *rsa.PublicKey
+}
+
+func (kt *keysType) getKey(kid string) *rsa.PublicKey {
+	if key := kt.m[kid]; key != nil {
+		return key
+	}
+	return kt.first
+}
 
 const (
 	authenticationPubKeySecretPath = "secret/authentication/public-key"
 	jwtAlg                         = "RS256"
 )
+
+// JWTHeaderKeyID is the JWT header for the key id,
+// as defined in RFC 7517 section 4.5.
+const JWTHeaderKeyID = "kid"
 
 // ErrNoPublicKeysLoaded is an error returned by ValidateToken indicates that
 // the function is called before any public keys are loaded from secrets.
@@ -28,7 +49,7 @@ var ErrEmptyToken = errors.New("edgecontext.ValidateToken: empty JWT token")
 // ValidateToken parses and validates a jwt token, and return the decoded
 // AuthenticationToken.
 func (impl *Impl) ValidateToken(token string) (*AuthenticationToken, error) {
-	keys, ok := impl.keysValue.Load().(keysType)
+	keys, ok := impl.keysValue.Load().(*keysType)
 	if !ok {
 		// This would only happen when all previous middleware parsing failed.
 		return nil, ErrNoPublicKeysLoaded
@@ -48,8 +69,9 @@ func (impl *Impl) ValidateToken(token string) (*AuthenticationToken, error) {
 	tok, err := jwt.ParseWithClaims(
 		token,
 		&AuthenticationToken{},
-		func(_ *jwt.Token) (interface{}, error) {
-			return keys, nil
+		func(jt *jwt.Token) (interface{}, error) {
+			kid, _ := jt.Header[JWTHeaderKeyID].(string)
+			return keys.getKey(kid), nil
 		},
 	)
 	if err != nil {
@@ -85,26 +107,55 @@ func (impl *Impl) validatorMiddleware(next secrets.SecretHandlerFunc) secrets.Se
 			return
 		}
 
-		all := versioned.GetAll()
-		keys := make(keysType, 0, len(all))
-		for i, v := range all {
-			key, err := jwt.ParseRSAPublicKeyFromPEM([]byte(v))
-			if err != nil {
-				impl.logger.Log(context.Background(), fmt.Sprintf(
-					"Failed to parse key #%d: %v",
+		keys := parseVersionedKeys(context.Background(), versioned, impl.logger)
+		if keys != nil {
+			impl.keysValue.Store(keys)
+		}
+	}
+}
+
+func parseVersionedKeys(ctx context.Context, versioned secrets.VersionedSecret, logger log.Wrapper) *keysType {
+	all := versioned.GetAll()
+	keys := &keysType{
+		m: make(map[string]*rsa.PublicKey, len(all)),
+	}
+	for i, v := range all {
+		key, err := jwt.ParseRSAPublicKeyFromPEM([]byte(v))
+		if err != nil {
+			logger.Log(ctx, fmt.Sprintf(
+				"Failed to parse key #%d: %v",
+				i,
+				err,
+			))
+		} else {
+			if keys.first == nil {
+				keys.first = key
+			}
+			if fingerprint, err := RSAPublicKeyFingerprint(key); err != nil {
+				logger.Log(ctx, fmt.Sprintf(
+					"Failed to get fingerprint of key #%d: %v",
 					i,
 					err,
 				))
 			} else {
-				keys = append(keys, key)
+				keys.m[fingerprint] = key
 			}
 		}
-
-		if len(keys) == 0 {
-			impl.logger.Log(context.Background(), "No valid keys in secrets store.")
-			return
-		}
-
-		impl.keysValue.Store(keys)
 	}
+	if keys.first == nil {
+		logger.Log(ctx, "No valid keys in secrets store.")
+		return nil
+	}
+	return keys
+}
+
+// RSAPublicKeyFingerprint calculates the fingerprint of an RSA public key,
+// using ssh.FingerprintSHA256:
+// https://pkg.go.dev/golang.org/x/crypto/ssh#FingerprintSHA256
+func RSAPublicKeyFingerprint(pubKey *rsa.PublicKey) (string, error) {
+	key, err := ssh.NewPublicKey(pubKey)
+	if err != nil {
+		return "", err
+	}
+	return ssh.FingerprintSHA256(key), nil
 }
--- lib/go/edgecontext/validator_test.go
+++ lib/go/edgecontext/validator_test.go
@@ -2,6 +2,10 @@ package edgecontext_test
 
 import (
 	"testing"
+
+	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/reddit/edgecontext/lib/go/edgecontext"
 )
 
 // copied from https://github.com/reddit/edgecontext.py/blob/420e58728ee7085a2f91c5db45df233142b251f9/tests/edge_context_tests.py#L54
@@ -16,5 +20,33 @@ func TestValidToken(t *testing.T) {
 	actual := token.Subject()
 	if actual != expected {
 		t.Errorf("subject expected %q, got %q", expected, actual)
+	}
+}
+
+const (
+	testPubKeyPEM = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtzMnDEQPd75QZByogNlB
+NY2auyr4sy8UNTDARs79Edq/Jw5tb7ub412mOB61mVrcuFZW6xfmCRt0ILgoaT66
+Tp1RpuEfghD+e7bYZ+Q2pckC1ZaVPIVVf/ZcCZ0tKQHoD8EpyyFINKjCh516VrCx
+KuOm2fALPB/xDwDBEdeVJlh5/3HHP2V35scdvDRkvr2qkcvhzoy0+7wUWFRZ2n6H
+TFrxMHQoHg0tutAJEkjsMw9xfN7V07c952SHNRZvu80V5EEpnKw/iYKXUjCmoXm8
+tpJv5kXH6XPgfvOirSbTfuo+0VGqVIx9gcomzJ0I5WfGTD22dAxDiRT7q7KZnNgt
+TwIDAQAB
+-----END PUBLIC KEY-----`
+
+	expectedFingerprint = "SHA256:lZ0hkWRsDpapeBu2ekX9WY2oYInHwdRaXTwtBecDicI"
+)
+
+func TestFingerprint(t *testing.T) {
+	pubKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(testPubKeyPEM))
+	if err != nil {
+		t.Fatalf("Unable to parse pub key from PEM: %v", err)
+	}
+	fingerprint, err := edgecontext.RSAPublicKeyFingerprint(pubKey)
+	if err != nil {
+		t.Errorf("Unable to calculate fingerprint from pub key: %v", err)
+	}
+	if fingerprint != expectedFingerprint {
+		t.Errorf("Fingerprint got %q, want %q", fingerprint, expectedFingerprint)
 	}
 }
