8d1873c18df08c1cd702ba5d502be75988733784
r/Salomon Smeke <134332337+salomon-smekecohen@users.noreply.github.com>
2025-08-12 12:42:58 -0500

    Swap thrift-unofficial for thrift (#33)
    thrift-unofficial is no longer needed, as thrift publishes to pypi frequently. moreover, thrift-unofficial has not been published in quite some time

--- lib/py/setup.py
+++ lib/py/setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         "baseplate>=1.5,<3.0",
         "pyjwt>=2.0.0,<3.0",
-        "thrift-unofficial>=0.14,<1.0",
+        "thrift>=0.14,<1.0",
         "cryptography>=3.0",
     ],
     package_data={"reddit_edgecontext": ["py.typed"]},
